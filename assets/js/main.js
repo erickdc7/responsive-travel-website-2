9b80f83749e5c2c10edd10007f2433c160126f7a
ErickDC <erickdiazc717@gmail.com>
2023-08-05 13:45:06 -0500

    blur header function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,14 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >= 50
+        ? header.classList.add('blur-header')
+        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
 
