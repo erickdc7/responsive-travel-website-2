33bfef0810cfb662f2ae3e3a4068a053560d742e
ErickDC <erickdiazc717@gmail.com>
2023-08-14 11:35:22 -0500

    footer__container animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ const sr = ScrollReveal({
     //reset:true // Animations repeat
 })
 
-sr.reveal(`.home__data, .explore__data, .explore__user`)
+sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
 sr.reveal(`.home__card`, { delay: 600, distance: '100px', interval: 100 })
 sr.reveal(`.about__data, .join__image`, { origin: 'right' })
 sr.reveal(`.about__image, .join__data`, { origin: 'left' })
